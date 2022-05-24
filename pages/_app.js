809a0fcbfde3c046dc41d6a558333e408ccf8ac9
asdqwee <layerlain@yandex.ru>
2022-05-25 02:07:34 +0900

    feat: verstka

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // <>
+    //   <Head>
+
+    //     <link href="../assets/San Francisco Pro Text/SF-Pro-Display-Black.otf" rel="stylesheet" />
+
+    //   </Head>
+    <Component {...pageProps} />
+  );
 }
 
-export default MyApp
+export default MyApp;
