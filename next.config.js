e2c76a1eb5bfdeec946dc20ff80b494c76bb5de4
asdqwee <layerlain@yandex.ru>
2022-05-25 21:53:49 +0900

    feat: verstka finished

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
+// next.config.js
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const withImages = require('next-images');
+module.exports = withImages();
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,98 @@
 import Image from 'next/image';
+import ArrowPopup from '../public/static/img/Vector.svg';
+import Search from '../public/static/img/Group.svg';
+import HomeLine from '../public/static/img/home-line.svg';
+import CalendarLine from '../public/static/img/calendar-line.svg';
+import ArrowDropDown from '../public/static/img/arrow-drop-down-line.svg';
+import User from '../public/static/img/user.svg';
 
 export default function Home() {
   return (
-    <div className="app">
-      <div className="overlay">
-        <div className="container">
-          <header>
-            <div className="group">
-              <ul className="navigation">
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </div>
-            <div className="group">
-              <ul className="camping">
-                <li>Camping.</li>
-              </ul>
-            </div>
-            <div className="group">
-              <ul className="registration">
-                <li>Sign In</li>
-                <li>Sign Up</li>
-                <li>
-                  <Image
-                    src="/public/static/img/Group.svg"
-                    alt="Landscape picture"
-                    width={24}
-                    height={24}
-                  />
-                </li>
-              </ul>
-            </div>
-          </header>
-          <div className="banner">
-            <div className="banner__title">
-              <h1>Beautiful places of England</h1>
-            </div>
-            <div className="banner__subtitle">
-              <h2>
-                Plan your vacation on the most beatiful <br></br> places of England
-              </h2>
-            </div>
+    <div className="wrapper">
+      <div className="content">
+        <header>
+          <div className="group">
+            <ul className="navigation">
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
           </div>
-          <div className="popup"></div>
-          <div className="book">
-            <div className="book__title">Book your vacation</div>
-            <div className="book__content">
-              <div className="book__panel_wrapper">
-                <div className="accomodation">
-                  <p>6730 Luna Land North Rhiannonmouth</p>
+          <div className="group">
+            <ul className="camping">
+              <li>Camping.</li>
+            </ul>
+          </div>
+          <div className="group">
+            <ul className="registration">
+              <li>Sign In</li>
+              <li>Sign Up</li>
+              <li>
+                <img src={Search}></img>
+              </li>
+            </ul>
+          </div>
+        </header>
+        <div className="banner">
+          <div className="banner__title">
+            <h1>Beautiful places of England</h1>
+          </div>
+          <div className="banner__subtitle">
+            <h2>
+              Plan your vacation on the most beatiful <br></br> places of England
+            </h2>
+          </div>
+        </div>
+        <div className="popup">
+          <div>
+            <div className="popup__subtitle">Up next</div>
+            <div className="popup__title">Aerial View of Rock Cliffs</div>
+            <img src={ArrowPopup}></img>
+          </div>
+          <div className="popup__bg"></div>
+        </div>
+        <div className="book">
+          <div className="book__title">Book your vacation</div>
+          <div className="book__content">
+            <div className="book__panel_wrapper">
+              <div className="accomodation">
+                <div className="book_item__wrapper">
+                  <img src={HomeLine}></img>
+                  <div className="book_item__content">
+                    <p className="book_item__title">Accommodation</p>
+                    <p>6730 Luna Land North Rhiannonmouth</p>
+                  </div>
                 </div>
-                <div className="checkin">19.06.2019</div>
-                <div className="checkout">19.06.2019</div>
-                <div className="guests">4 adults</div>
+                <img src={ArrowDropDown}></img>
               </div>
-
-              <button className="btn">Search</button>
+              <div className="checkin">
+                <div className="book_item__wrapper">
+                  <img src={CalendarLine}></img>
+                  <div className="book_item__content">
+                    <p className="book_item__title">Check-in</p>
+                    <p>19.06.2019</p>
+                  </div>
+                </div>
+              </div>
+              <div className="checkout">
+                <div className="book_item__wrapper">
+                  <div className="book_item__content">
+                    <p className="book_item__title">Check-out</p>
+                    <p>19.06.2019</p>
+                  </div>
+                </div>
+              </div>
+              <div className="guests">
+                <div className="book_item__wrapper">
+                  <img src={User}></img>
+                  <div className="book_item__content">
+                    <p className="book_item__title">Guests</p>
+                    <p>4 adults</p>
+                  </div>
+                </div>{' '}
+                <img src={ArrowDropDown}></img>
+              </div>
             </div>
+            <button className="btn">Search</button>
           </div>
         </div>
       </div>
